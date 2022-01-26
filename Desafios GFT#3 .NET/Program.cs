3fca2883b688d9eaa70ea89308118e0187484284
Rosembergg <rrosembergg.45@gmail.com>
2022-01-26 15:20:27 -0300

    Desafio GTF .NET

---
+++ Desafios GFT#3 .NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafios_GFT_3_.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
